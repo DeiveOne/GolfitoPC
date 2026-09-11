be4a5d53611f43acedb9dc3ae16cb77996904252
HarolPerezTrujillo <haroldpereztrujillo@gmail.com>
2026-09-11 08:50:31 -0500

    Implement accelerometer sensor and swing detection

--- app/src/main/java/com/example/golfitopc/MainActivity.kt
+++ app/src/main/java/com/example/golfitopc/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import com.example.golfitopc.game.GameEngine
 import com.example.golfitopc.game.GameState
+import com.example.golfitopc.sensor.SensorManagerHelper
 import com.example.golfitopc.ui.GameScreen
 import com.example.golfitopc.ui.theme.GolfitoPCTheme
 
@@ -26,10 +28,31 @@ class MainActivity : ComponentActivity() {
                     mutableStateOf(GameState())
                 }
 
+                val sensorHelper = remember {
+                    SensorManagerHelper(this) { force, direction ->
+
+                        gameState.value = GameEngine.applyShot(
+                            state = gameState.value,
+                            force = force,
+                            directionDegrees = direction
+                        )
+                    }
+                }
+
+                DisposableEffect(Unit) {
+
+                    sensorHelper.start()
+
+                    onDispose {
+                        sensorHelper.stop()
+                    }
+                }
+
                 GameScreen(
                     state = gameState.value,
 
                     onShot = {
+                        // Golpe de prueba
                         gameState.value = GameEngine.applyShot(
                             state = gameState.value,
                             force = 0.8f,
